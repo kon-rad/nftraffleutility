5a637434e81d6eb62edb479afabc01639b6b8e14
Konrad Gnat <konradgnat@Konrads-MacBook-Pro-2.local>
2022-05-27 22:16:05 -0700

    implement buying raffle tickets, transferring nft

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract VRFD20 is VRFConsumerBaseV2 {
-
-}
-
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, ERC721Holder {
     using SafeMath for uint256;
 
     mapping(uint256 => address) private s_rollers;
@@ -22,6 +20,8 @@ contract Raffle is VRFConsumerBaseV2 {
     uint16 requestConfirmations = 3;
     uint32 numWords =  1;
     VRFCoordinatorV2Interface COORDINATOR;
+    address public admin;
+    uint256 public adminFeePercent = 3;
 
     enum Status {
         Active,
@@ -35,6 +35,7 @@ contract Raffle is VRFConsumerBaseV2 {
         address owner;
         Status status;
         uint256 winnerIndex;
+        uint256 balance;
         address prizeNFTAddress;
         uint256 prizeNFTTokenId;
         uint256 ticketPrice;
@@ -45,22 +46,18 @@ contract Raffle is VRFConsumerBaseV2 {
         address[] tickets;
     }
 
-    address public admin;
-    uint256 public adminFeePercent;
-    uint256 public adminBalance;
-
     mapping(uint256 => Raffle) public raffles;
-    mapping(uint256 => uint256) public raffleToRandomRequestId;
+    mapping(uint256 => uint256) public requestIdToRaffle;
     uint256 rafflesCount = 0;
 
     event RaffleCreated(uint256 id, string name);
-    event RaffleFinished(uint256 id, string name);
+    event RaffleFinished(uint256 id, string name, uint256 winnerIndex);
     event RafflePending(uint256 id, string name, uint256 requestId);
 
     constructor(uint64 _subscriptionId, address _vrfCoordinator, bytes32 _keyHash) VRFConsumerBaseV2(_vrfCoordinator) {
         vrfCoordinator = _vrfCoordinator;
         keyHash = _keyHash;
-        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
         admin = msg.sender;
         subscriptionId = _subscriptionId;
     }
@@ -97,29 +94,34 @@ contract Raffle is VRFConsumerBaseV2 {
     function createRaffle(
         string memory _name,
         string memory _description,
-        address _nftAddress,
-        uint256 _nftTokenId,
-        uint256 _prizePercentage,
+        address _prizeNFTAddress,
+        uint256 _prizeNFTTokenId,
         uint256 _ticketPrice,
         uint256 _ticketGoal,
         uint256 _raffleDurationDays
     ) public {
         require(_ticketGoal >= 10, "Ticket goal must be 10 or greater");
         require(_ticketGoal <= 1000000, "Ticket goal must be less than 1,000,000");
-        Raffle storage raffle = Raffle({
-            status: Status.Active,
-            name: _name,
-            description: _description,
-            owner: msg.sender,
-            nftAddress: _nftAddress,
-            nftTokenId: _nftTokenId,
-            ticketPrice: _ticketPrice,
-            ticketGoal: _ticketGoal,
-            startDate: block.timestamp,
-            endDate: block.timestamp + _raffleDurationDays * 1 days,
-            tickets: []
-        });
-        rafflesCount++;
+
+        // transfer NFT to raffle contract
+        IERC721(_prizeNFTAddress).safeTransferFrom(msg.sender, address(this), _prizeNFTTokenId);
+
+        Raffle storage raffle = raffles[rafflesCount++];
+
+        raffle.status = Status.Active;
+        raffle.name = _name;
+        raffle.name = _description;
+        raffle.owner = msg.sender;
+        raffle.balance = 0;
+        raffle.prizeNFTAddress = _prizeNFTAddress;
+        raffle.prizeNFTTokenId = _prizeNFTTokenId;
+        raffle.ticketPrice = _ticketPrice;
+        raffle.ticketGoal = _ticketGoal;
+        raffle.startDate = block.timestamp;
+        raffle.endDate = block.timestamp + _raffleDurationDays * 1 days; 
+        raffle.raffleTotal = 0;
+        raffle.tickets;
+
         emit RaffleCreated(rafflesCount, _name);
     }
 
@@ -127,8 +129,7 @@ contract Raffle is VRFConsumerBaseV2 {
         return raffles[_raffleId];
     }
 
-    function checkWinner(uint256 _raffleId) public view returns (address) {
-        require(raffles[_raffleId].status == Status.Finished, "raffle is not finished");
+    function getWinner(uint256 _raffleId) public onlyFinished(_raffleId) view returns (address) {
         return raffles[_raffleId].tickets[raffles[_raffleId].winnerIndex];
     }
 
@@ -140,7 +141,6 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function _finishRaffle(uint256 _raffleId) internal {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
         Raffle storage raffle = raffles[_raffleId];
         if (raffle.tickets.length == 0) {
             raffle.status = Status.Finished;
@@ -155,52 +155,38 @@ contract Raffle is VRFConsumerBaseV2 {
             numWords
         );
         raffle.status = Status.Pending;
-        raffleToRandomRequestId[requestId] = _raffleId;
+        requestIdToRaffle[requestId] = _raffleId;
         emit RafflePending(_raffleId, raffle.name, requestId);
     }
     
     function fulfillRandomWords(
-        uint256, requestId
+        uint256 requestId,
         uint256[] memory randomWords
     ) internal override {
-        s_randomWords = randomWords;
-    }
-
-    function _finishRaffle(uint256 _raffleId) internal {
-        require(
-            raffles[_raffleId].tickets.length > 0,
-            "The raffle didnt have any tickets"
-        );
+        uint256 _raffleId = requestIdToRaffle[requestId];
         Raffle storage raffle = raffles[_raffleId];
-        uint256 winnerId = _generateRandomNumber(raffle.tickets.length);
-        raffle.status = Status.Finished;
-        raffle.winner = winnerId;
-        raffle.endDate = block.timestamp;
-        emit RaffleFinished(_raffleId, raffle.name);
+        raffle.winnerIndex = randomWords[0];
+        address winner = raffle.tickets[randomWords[0]];
+
+        // transfer NFT to winner
+        IERC721(raffle.prizeNFTAddress).safeTransferFrom(address(this), address(winner), raffle.prizeNFTTokenId);
+
+        // send raffle funds to owner
+        uint256 adminShare = raffle.balance.div(100).mul(adminFeePercent);
+        payable(address(admin)).transfer(adminShare);
+        payable(address(raffle.owner)).transfer(raffle.balance.sub(adminShare));
+
+        emit RaffleFinished(_raffleId, raffle.name, raffle.winnerIndex);
     }
 
-    // Request randomness
-    function getRandomNumber() public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
-        return requestRandomness(keyHash, fee);
-    }
-
-
-    // Assumes the subscription is funded sufficiently.
-    function requestRandomWords() external onlyOwner {
-        // Will revert if subscription is not set and funded.
-        s_requestId = COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
-    }
-
-
-    // Callback function used by VRF Coordinator
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
+    function buyRaffleTicket(uint256 _raffleId, uint256 _numOfTickets) public onlyActive(_raffleId) payable {
+        require(_numOfTickets > 0, "Must buy at least one ticket");
+        require(_numOfTickets < raffles[_raffleId].tickets.length + raffles[_raffleId].ticketGoal, "Not enough tickets to sell");
+        require(_numOfTickets.mul(raffles[_raffleId].ticketPrice) <= msg.value, "Not enough to pay for tickets");
+        raffles[_raffleId].balance = raffles[_raffleId].balance.add(_numOfTickets.mul(raffles[_raffleId].ticketPrice));
+        uint256 i = 0;
+        for (i; i < _numOfTickets; i++) {
+            raffles[_raffleId].tickets.push(msg.sender);
+        }
     }
 }
